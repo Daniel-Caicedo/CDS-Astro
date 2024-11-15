205dff193792d698bf787d904134ab42fea50650
Daniel Caicedo <danielfcaicedootmail.com>
2024-11-15 17:07:07 -0500

    creacion de ts y de mas

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,5 +1,3 @@
-import ogImageSrc from "@images/social.png";
-
 export const SITE = {
   title: "Cordon de seguridad",
   tagline: "Vigilancia y seguridad de primera",
@@ -35,5 +33,4 @@ export const OG = {
   url: SITE.url,
   title: `${SITE.title}: : Servicios de seguridad movil , vigilancia escolta`,
   description: "Protege tu negocio o proyectos con los servicios de primera de Cordon de seguridad . Con la confianza de lideres de la industria, Cordon  ofrece sencillez, asequibilidad, y fiabilidad. Experimente la diferencia con un servicio centrado en el usuario y herramientas de vanguardia. ¡Empieza a explorar ahora!",
-  image: ogImageSrc,
 };
